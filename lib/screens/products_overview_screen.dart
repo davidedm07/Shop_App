0fffe7a57874f91f7759f01088eb2b37b07a402c
Davide Di Maria <davidedimaria@rentalcars.com>
2021-02-19 16:58:38 +0000

    Shop_App: starting to create the model

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../models/product.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
